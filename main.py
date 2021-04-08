e553ea36b984b4b4785e84de6ebe991bfbe8d6d6
Redmoons <sjbyeon610@gmail.com>
2021-04-08 21:14:24 +0900

    update main

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
-from kafka import KafkaConsumer
+from confluent_kafka import Consumer
 import json
+from datetime import datetime
 
 class Main:
     def __init__(self):
-        pass
+        conf = {'bootstrap.servers': "localhost:9092",
+        'group.id': "group1",
+        'auto.offset.reset': 'earliest'}
+        self.consumer = Consumer(conf)
+        self.running = True
+        self.min_commit_count = 100
 
-    def execute(self):
-        print("start main.py")
+    def consume_loop(consumer, topics):
+        try:
+            consumer.subscribe(topics)
 
-        # extractor - consumer
-        consumer = KafkaConsumer('stockmarket', bootstrap_servers='localhost:9092', max_poll_records=100)
+            msg_count = 0
+            # extractor - consumer
+            while running:
+                msg = consumer.poll(timeout=1.0)
+                if msg is None: 
+                    continue
 
-        # while True:
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        # End of partition event
+                        sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
+                                        (msg.topic(), msg.partition(), msg.offset()))
+                    elif msg.error():
+                        raise KafkaException(msg.error())
+                else:
+                    # transformer - change data
+                    # msg_process(msg)
 
-        for msg in consumer:                                                                                                                                                                                    
-            m = json.loads(msg[6].decode('utf-8'))                                                                                                                                                              
-            print(m['text'])
+                    # loader - save to couchbase - nosql
+                    msg_count += 1
+                    if msg_count % min_commit_count == 0:
+                        consumer.commit(async=False)
+        finally:
+            # Close down consumer to commit final offsets.
+            consumer.close()
 
-
-        # transformer - change data
-
-        # loader - save to couchbase - nosql
+    def shutdown():
+        running = False
 
 if __name__ == "__main__":
     main = Main()
-    main.execute()
+    main.consume_loop("stockmarket")
